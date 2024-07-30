6d30e443a329c1f81283f6d80db7f35b54a42b21
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-07-30 14:13:57 +0200

    debugging

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.post("/result", async (req, res) => {
       console.log(newTrip);
       db.trips.push(newTrip);
       res.send(newResults);
-      //res.send(newTrip);
+      res.send(newTrip);
     })
     .catch((error) => {
       console.log(error.response);
